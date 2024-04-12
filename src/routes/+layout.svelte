bbdd23b139d66dfaee687b6784281313a4dc039a
sara hzs <sara.h.sarkhanloo@vougee.gmbh>
2024-04-12 10:47:46 +0200

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,12 @@
 
 <style>
 
-    *,
-    *::before,
-    *::after {
+
+    html {
+ 
         margin: 0;
         padding: 0;
         box-sizing: inherit;
-    }
-
-    html {
-        box-sizing: border-box;
     }
 
     body {
